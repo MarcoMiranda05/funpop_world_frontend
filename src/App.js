53ac1d7210f50d11e04a4ea6615f9325580d5f63
MarcoMiranda05 <marmirand@hotmail.com>
2019-07-16 16:38:49 +0100

    fetching all funkos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from "./images/FunPop_logo.png";
 import "./App.css";
 import NavBar from "./components/NavBar";
+const API = "http://localhost:3000";
 
 class App extends Component {
+  state = {
+    funkos: []
+  };
+
+  componentDidMount() {
+    this.fetchFunkos();
+  }
+
+  ////// ------------- funkos request ------------------- /////////////
+
+  fetchFunkos = () => {
+    fetch(`${API}/funkos`)
+      .then(resp => resp.json())
+      .then(data => this.setState({ funkos: data }));
+  };
+
+  ////// ------------- routes ------------------- /////////////
+
   home = () => {
     return <div>Hello FunPop! WORLD</div>;
   };
